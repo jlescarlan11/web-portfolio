c1e82207d761c9a739887d64bc3525be656b74bc
John Lester Escarlan <jlescarlan11@gmail.com>
2024-05-26 09:42:38 +0800

    edited the layout of my navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -6,24 +6,25 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Navbar = () => {
   return (
     <div className="fixed w-svw bg-bg-dark/10 border-b border-text-dark/10">
-      <nav className="sticky flex flex-row justify-around max-md:justify-between max-md:px-16 items-center font-semibold text-base p-[1rem]">
+      <nav className="sticky flex flex-row justify-between px-[2rem] items-center font-normal text-base py-[1rem]">
         <div>
           <a href="#home" className="flex">
             LEST
           </a>
         </div>
-        <div className="flex flex-row justify-center items-center gap-[1rem] max-md:hidden">
+        <div className="flex flex-row justify-center items-center gap-[1rem]">
           {navigation.map((item) => (
-            <a key={item.id} href={item.url}>
+            <a key={item.id} href={item.url} className="max-md:hidden">
               {item.title}
             </a>
           ))}
-        </div>
-        <div className="max-md:hidden">
-          <FontAwesomeIcon icon={faMoon} className="size-[1.2rem] flex" />
-        </div>
-        <div className="md:hidden">
-          <FontAwesomeIcon icon={faBars} className="size-[1.2rem] flex" />
+          <div className="border-l-2 border-text-dark/10 py-[1rem] max-md:hidden"></div>
+          <div className="max-md:hidden">
+            <FontAwesomeIcon icon={faMoon} className="size-[1rem] flex" />
+          </div>
+          <div className="md:hidden">
+            <FontAwesomeIcon icon={faBars} className="size-[1rem] flex" />
+          </div>
         </div>
       </nav>
     </div>
